227737734960e461e58a748ab888b92cf75c305a
altern <sergey.shmarkatyuk@gmail.com>
2014-04-30 00:45:36 -0700

    implemented configurable basic charts

--- source/js/cs519/multiSeriesLineChart.js
+++ source/js/cs519/multiSeriesLineChart.js
@@ -1,91 +1,235 @@
 function multiSeriesLineChart() {
-    var margin = {top: 20, right: 80, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    
+    var margin = {top: 20, right: 20, bottom: 30, left: 50},
+        width = 1000,
+        height = 500,
+        xLabel = '',
+        yLabel = "Temperature (ºF)",
+        xScale = d3.scale.linear(),
+        yScale = d3.scale.linear(),
+        xFormat = d3.time.format('%B'),
+        yFormat = d3.format(),
+        xColumnName = 'date',
+        yColumnName = 'temperature',
+        xParseFormat = d3.time.format("%Y%m%d"),
+        xColumnOrdering = 'ascending',
+        csvFile = "data/temperatures.csv",
+        chartId = '#visualization';
 
-    var parseDate = d3.time.format("%Y%m%d").parse;
+    var chart = function() {
 
-    var x = d3.time.scale()
-        .range([0, width]);
+        var x = xScale
+            .range([0, width - margin.left - margin.right]);
 
-    var y = d3.scale.linear()
-        .range([height, 0]);
+        var y = yScale
+            .range([height - margin.top - margin.bottom, 0]);
 
-    var color = d3.scale.category10();
+        var color = d3.scale.category10();
 
-    var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom");
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom")
+            .tickFormat(xFormat);
 
-    var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left");
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left")
+            .tickFormat(yFormat);
 
-    var line = d3.svg.line()
-        .interpolate("basis")
-        .x(function(d) { return x(d.date); })
-        .y(function(d) { return y(d.temperature); });
+        var line = d3.svg.line()
+            .interpolate("basis")
+            .x(function(d) { return x(d[xColumnName]); })
+            .y(function(d) { return y(d[yColumnName]); });
 
-    var svg = d3.select('#visualization').append('div').attr('id', 'multiSeriesLineChart').append('svg')
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        d3.select(chartId).remove()
 
-    d3.tsv("data/temperatures.tsv", function(error, data) {
-      color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
+        var svg = d3.select('body').append('div').attr('id', 'visualization')
+            .append('div').attr('id', 'multiSeriesLineChart')
+            .append('svg')
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-      data.forEach(function(d) {
-        d.date = parseDate(d.date);
-      });
+        d3.csv(csvFile, function(error, csv) {
+            if(xParseFormat.parse) {
+                csv.sort(function(a, b) {return d3[xColumnOrdering].call(d3, xParseFormat.parse(a[xColumnName]), xParseFormat.parse(b[xColumnName]));})
+            } else {
+                csv.sort(function(a, b) {return d3[xColumnOrdering].call(d3, xFormat(a[xColumnName]), xFormat(b[xColumnName]));})
+            }
 
-      var cities = color.domain().map(function(name) {
-        return {
-          name: name,
-          values: data.map(function(d) {
-            return {date: d.date, temperature: +d[name]};
-          })
-        };
-      });
+            color.domain(d3.keys(csv[0]).filter(function(key) { return key !== xColumnName; }));
 
-      x.domain(d3.extent(data, function(d) { return d.date; }));
+            csv.forEach(function(d) {
+                if(xParseFormat.parse) {
+                    d[xColumnName] = xParseFormat.parse(d[xColumnName]);
+                } else {
+                    d[xColumnName] = xFormat(d[xColumnName]);
+                }
+            });
 
-      y.domain([
-        d3.min(cities, function(c) { return d3.min(c.values, function(v) { return v.temperature; }); }),
-        d3.max(cities, function(c) { return d3.max(c.values, function(v) { return v.temperature; }); })
-      ]);
+            var colors = color.domain().map(function(name) {
+              return {
+                name: name,
+                values: csv.map(function(d) {
+                  return {date: d[xColumnName], temperature: +d[name]};
+                })
+              };
+            });
 
-      svg.append("g")
-          .attr("class", "x axis")
-          .attr("transform", "translate(0," + height + ")")
-          .call(xAxis);
+            x.domain(d3.extent(csv, function(d) { return d[xColumnName]; }));
 
-      svg.append("g")
-          .attr("class", "y axis")
-          .call(yAxis)
-        .append("text")
-          .attr("transform", "rotate(-90)")
-          .attr("y", 6)
-          .attr("dy", ".71em")
-          .style("text-anchor", "end")
-          .text("Temperature (ºF)");
+            y.domain([
+              d3.min(colors, function(c) { return d3.min(c.values, function(v) { return v[yColumnName]; }); }),
+              d3.max(colors, function(c) { return d3.max(c.values, function(v) { return v[yColumnName]; }); })
+            ]);
 
-      var city = svg.selectAll(".city")
-          .data(cities)
-        .enter().append("g")
-          .attr("class", "city");
+            svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + height + ")")
+                .call(xAxis)
+                .append("text")
+                .attr("x", width)
+                .attr("dy", ".71em")
+                .attr("font-size", "24")
+                .style("text-anchor", "end")
+                .text(xLabel);
 
-      city.append("path")
-          .attr("class", "line")
-          .attr("d", function(d) { return line(d.values); })
-          .style("stroke", function(d) { return color(d.name); });
+            svg.append("g")
+                .attr("class", "y axis")
+                .call(yAxis)
+                .append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6)
+                .attr("dy", "1em")
+                .style("text-anchor", "end")
+                .attr("font-size", "24")
+                .text(yLabel);
 
-      city.append("text")
-          .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
-          .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")"; })
-          .attr("x", 3)
-          .attr("dy", ".35em")
-          .text(function(d) { return d.name; });
-    });
+            var col = svg.selectAll(".col")
+                .data(colors)
+                .enter().append("g")
+                .attr("class", "col");
 
+            col.append("path")
+                .attr("class", "line")
+                .attr("d", function(d) { return line(d.values); })
+                .style("stroke", function(d) { return color(d.name); });
+
+            col.append("text")
+                .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
+                .attr("transform", function(d) { return "translate(" + x(d.value[xColumnName]) + "," + y(d.value[yColumnName]) + ")"; })
+                .attr("x", 3)
+                .attr("dy", ".35em")
+                .text(function(d) { return d.name; });
+        });
+    }
+    function X(d) {
+        return xScale(d[0]);
+    }
+
+    // The x-accessor for the path generator; yScale ∘ yValue.
+    function Y(d) {
+        return yScale(d[1]);
+    }
+
+    chart.margin = function(_) {
+        if (!arguments.length)
+            return margin;
+        margin = _;
+        return chart;
+    };
+
+    chart.width = function(_) {
+        if (!arguments.length)
+            return width;
+        width = _;
+        return chart;
+    };
+
+    chart.height = function(_) {
+        if (!arguments.length)
+            return height;
+        height = _;
+        return chart;
+    };
+    
+    chart.xFormat  = function(_) {
+        if (!arguments.length)
+            return xFormat;
+        xFormat = _;
+        return chart;
+    };
+    
+    chart.xParseFormat  = function(_) {
+        if (!arguments.length)
+            return xParseFormat;
+        xParseFormat = _;
+        return chart;
+    };
+    
+    chart.yFormat  = function(_) {
+        if (!arguments.length)
+            return yFormat;
+        yFormat = _;
+        return chart;
+    };
+    
+    chart.xScale  = function(_) {
+        if (!arguments.length)
+            return xScale;
+        xScale = _;
+        return chart;
+    };
+    
+    chart.yScale  = function(_) {
+        if (!arguments.length)
+            return yScale;
+        yScale = _;
+        return chart;
+    };
+    
+    chart.xLabel  = function(_) {
+        if (!arguments.length)
+            return xLabel;
+        xLabel = _;
+        return chart;
+    };
+    
+    chart.yLabel  = function(_) {
+        if (!arguments.length)
+            return yLabel;
+        yLabel = _;
+        return chart;
+    };
+    
+    chart.xColumnName  = function(_) {
+        if (!arguments.length)
+            return xColumnName;
+        xColumnName = _;
+        return chart;
+    };
+    
+    chart.xColumnOrdering  = function(_) {
+        if (!arguments.length)
+            return xColumnOrdering;
+        xColumnOrdering = _;
+        return chart;
+    };
+    
+    chart.csvFile  = function(_) {
+        if (!arguments.length)
+            return csvFile;
+        csvFile = _;
+        return csvFile;
+    };
+    
+    chart.chartId  = function(_) {
+        if (!arguments.length)
+            return chartId;
+        chartId = _;
+        return chartId;
+    };
+
+    return chart;
 }
--- source/js/cs519/stackedAreaChart.js
+++ source/js/cs519/stackedAreaChart.js
@@ -2,37 +2,54 @@ function stackedAreaChart() {
     var margin = {top: 20, right: 20, bottom: 30, left: 50},
         width = 1000,
         height = 500,
-//        xScale = d3.time.scale(),
+        xLabel = '',
+        yLabel = "",
         xScale = d3.scale.linear(),
         yScale = d3.scale.linear(),
-//        xFormat = d3.time.format("%y-%b-%d").parse,
         xFormat = d3.format(),
         yFormat = d3.format(),
-        xColumnName = 'date';
-    
-    var chart = function(selection) {
-        var color = d3.scale.category20();
+        xColumnName = 'date',
+        yColumnName = 'date',
+        xParseFormat = d3.time.format('%y-%b-%d'),
+        xColumnOrdering = 'ascending',
+        csvFile = "data/browsers.csv",
+        chartId = '#visualization';
 
-        var stack = d3.layout.stack()
-            .values(function(d) { return d.values; });
+    var chart = function() {
+        
+        d3.csv(csvFile, function(error, csv) {
+            if(xParseFormat.parse) {
+                csv.sort(function(a, b) {return d3[xColumnOrdering].call(d3, xParseFormat.parse(a[xColumnName]), xParseFormat.parse(b[xColumnName]));})
+            } else {
+                csv.sort(function(a, b) {return d3[xColumnOrdering].call(d3, xFormat(a[xColumnName]), xFormat(b[xColumnName]));})
+            }
+            var color = d3.scale.category20();
 
-        var svg = d3.select('#visualization').append('div').attr('id', 'stackedAreaChart').append('svg') 
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            var stack = d3.layout.stack()
+                .values(function(d) { return d.values; });
 
-        var x = xScale
-            .range([0, width - margin.left - margin.right])
+            d3.select(chartId).remove()
 
-        var y = yScale
-            .range([height - margin.top - margin.bottom, 0])
-            
-        selection.each(function(data) {
-            color.domain(d3.keys(data[0]).filter(function(key) { 
+            var svg = d3.select('body').append('div').attr('id', 'visualization')
+                .append('div').attr('id', 'stackedAreaChart')
+                .append('svg') 
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+                .data(csv)
+
+            var x = xScale
+                .range([0, width - margin.left - margin.right])
+
+            var y = yScale
+                .range([height - margin.top - margin.bottom, 0])
+
+    //        selection.each(function(data) {
+            color.domain(d3.keys(csv[0]).filter(function(key) { 
                 return key !== xColumnName; 
             }));
-            
+
             var xAxis = d3.svg.axis()
                 .scale(x)
                 .orient("bottom")
@@ -42,27 +59,32 @@ function stackedAreaChart() {
                 .scale(y)
                 .orient("left")
                 .tickFormat(yFormat);
-                
+
             var area = d3.svg.area()
                 .x(function(d) { return x(d.x); })
                 .y0(function(d) { return y(d.y0); })
                 .y1(function(d) { return y(d.y0 + d.y); });
             
-            data.forEach(function(d) {
-                d[xColumnName] = xFormat(d[xColumnName]);
+            csv.forEach(function(d) {
+                if(xParseFormat.parse) {
+                    d[xColumnName] = xParseFormat.parse(d[xColumnName]);
+                } else {
+                    d[xColumnName] = xFormat(d[xColumnName]);
+                }
             });
+
             var areaData = stack(color.domain().map(function(name) {
                 return {
                     name: name,
-                    values: data.map(function(d) {
+                    values: csv.map(function(d) {
                         return {x: d[xColumnName], y: parseFloat(d[name])};
                     })
                 };
             }));
-            
-            x.domain(d3.extent(data, function(d) { return d[xColumnName]; }));
-            y.domain([0, d3.max(data.map(function(elem, i) {
-                return d3.sum(color.domain().map(function(name) { return data[i][name] }))
+
+            x.domain(d3.extent(csv, function(d) { return d[xColumnName]; }));
+            y.domain([0, d3.max(csv.map(function(elem, i) {
+                return d3.sum(color.domain().map(function(name) { return csv[i][name] }))
             }))])
 
             var singleArea = svg.selectAll(".singleArea")
@@ -85,12 +107,26 @@ function stackedAreaChart() {
             svg.append("g")
                 .attr("class", "x axis")
                 .attr("transform", "translate(0," + (height - margin.top - margin.bottom) + ")")
-                .call(xAxis);
+                .call(xAxis)
+                .append("text")
+                .attr("x", width)
+                .attr("dy", ".71em")
+                .attr("font-size", "24")
+                .style("text-anchor", "end")
+                .text(xLabel);
 
             svg.append("g")
                 .attr("class", "y axis")
-                .call(yAxis);
-        });
+                .call(yAxis)
+                .append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6)
+                .attr("dy", "1em")
+                .style("text-anchor", "end")
+                .attr("font-size", "24")
+                .text(yLabel);
+            }
+        );
     }
     
     function X(d) {
@@ -130,10 +166,45 @@ function stackedAreaChart() {
         return chart;
     };
     
+    chart.xParseFormat  = function(_) {
+        if (!arguments.length)
+            return xParseFormat;
+        xParseFormat = _;
+        return chart;
+    };
+    
     chart.yFormat  = function(_) {
         if (!arguments.length)
             return yFormat;
         yFormat = _;
+        return chart;
+    };
+    
+    chart.xLabel  = function(_) {
+        if (!arguments.length)
+            return xLabel;
+        xLabel = _;
+        return chart;
+    };
+    
+    chart.yLabel  = function(_) {
+        if (!arguments.length)
+            return yLabel;
+        yLabel = _;
+        return chart;
+    };
+    
+    chart.xScale  = function(_) {
+        if (!arguments.length)
+            return xScale;
+        xScale = _;
+        return chart;
+    };
+    
+    chart.yScale  = function(_) {
+        if (!arguments.length)
+            return yScale;
+        yScale = _;
         return chart;
     };
     
@@ -142,6 +213,34 @@ function stackedAreaChart() {
             return xColumnName;
         xColumnName = _;
         return chart;
+    };
+    
+    chart.yColumnName  = function(_) {
+        if (!arguments.length)
+            return yColumnName;
+        yColumnName = _;
+        return chart;
+    };
+    
+    chart.xColumnOrdering  = function(_) {
+        if (!arguments.length)
+            return xColumnOrdering;
+        xColumnOrdering = _;
+        return chart;
+    };
+    
+    chart.csvFile  = function(_) {
+        if (!arguments.length)
+            return csvFile;
+        csvFile = _;
+        return csvFile;
+    };
+    
+    chart.chartId  = function(_) {
+        if (!arguments.length)
+            return chartId;
+        chartId = _;
+        return chartId;
     };
 
     return chart;
